2abb10a9f321fff9a5363bb76ff48546737fbecc
InnaKokorina <93093046+InnaKokorina@users.noreply.github.com>
2022-03-23 17:56:52 +0400

    Initial Commit

---
+++ Instagram/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Inna Kokorina on 23.03.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
